0c46221acee4b27316744d22411f9b856a8f0f0e
Mohamed Bulhan <mustafa_1999@live.se>
2024-11-05 10:36:06 +0100

    first commit

---
+++ 6-scope.js
@@ -0,0 +1,19 @@
+// JavaScript scope determines the accessibility of variables within different parts of the code.
+
+// Task 1: Declare a variable called `globalVar` with the value "I am global".
+
+let globalVar = "I am global";
+
+// Task 2: Create a function called `testScope` that declares a variable called
+// `localVar` with the value "I am local".
+
+function testScope() {
+  let localVar = "I am local";
+  console.log("inside", globalVar, localVar);
+}
+
+// Task 3: Inside `testScope`, print both `globalVar` and `localVar`.
+testScope();
+// Task 4: Outside of the function, try to print `localVar` and observe the result.
+
+console.log("Outside", localVar);
